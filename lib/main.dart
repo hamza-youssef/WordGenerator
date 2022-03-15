69c7fc736e1866010a5003ff69289dfdf03a6568
unknown <hamza.youssef1919@gmail.com>
2022-03-15 01:25:54 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    return MaterialApp(
+      theme : ThemeData(primaryColor: Colors.purple[300]),
+      home: RandomWords()
+  
+  );
+  }
+}
+
